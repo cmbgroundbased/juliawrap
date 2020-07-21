e8f76cd2beb28bd27587e0bd03beb711d6f5c1ef
Stefano Mandelli <linuxfree2@gmail.com>
2020-07-21 22:50:41 +0200

    New feature (#1)
    * asd
    
    * Modify 1
    
    * Modify2

--- src/include/myLib.hpp
+++ src/include/myLib.hpp
@@ -1,21 +1,20 @@
 namespace myLib{
 
 class osservazione{
-	public:
-		osservazione();
 
-		void sum(double a, double b);
-		void diff(double a, double b);
-		
-		double a;
-		double b;
-	private:
-		double c;
-		double d;
+public:
+    osservazione();
+    void sum(double a, double b);
+    void diff(double a, double b);
+
+    double a;
+    double b;
+    double e;
+
+private:
+    double c;
+    double d;
+    double f;
 };
 
 }
-
-
-
-
